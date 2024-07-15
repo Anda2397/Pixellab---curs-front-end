ce1572e96712733b766cc45e7ed0746bc51446ce
Alexandra Iliescu <162130944+Anda2397@users.noreply.github.com>
2024-07-15 16:33:55 +0300

    Am adaugat exercitiul 2 din tema

--- JavaScript/Tema JS 10.js
+++ JavaScript/Tema JS 10.js
@@ -9,11 +9,50 @@ console.warn ("~~~~~~~~~~~ EXERCITIUL 1 ~~~~~~~~~~~")
 
 let array = [1, 2, 3, 4, 5, 6, 7, 8];
 let numar = 2; 
-console.log ("Array-ul initial este: " + array);
+console.log ("Array-ul initial contine: " + array);
 
 function  filtreazaDivizibile (array, numar) {
     return array % 2 ===  0;
 }
 
 const numereleDivizibile = array.filter (filtreazaDivizibile);
-console.log ("Array-ul care contine umerele divizibile cu 2 este: " + numereleDivizibile);
+console.log ("Array-ul care cuprinde numerele divizibile cu 2 din array-ul initial contine: " + numereleDivizibile);
+
+/* EX 2: Creaza o functie care primeste ca parametru 2 array-uri,
+primul va fi un array cu mai multe numere, al doilea un array cu mai putine numere.
+Scopul functiei este sa afle daca toate elementele din array-ul mic sunt prezente in array-ul mare,
+si sa returneze true sau daca nu sunt, sa returneze false.
+
+    Exemple:
+    Input: verificaIncluziuneArray ([1,2,3,4,5,6,7,8], [1, 2, 3]) 
+    Output true
+
+    Input: verificaIncluziuneArray ([1,2,3,4,5,6,7,8], [1, 2, 13]) 
+    Output false
+
+    Input: verificaIncluziuneArray ([1,2,3,4,5,6,7,8], [10, 13]) 
+    Output false
+*/
+console.warn ("~~~~~~~~~~~ EXERCITIUL 2 ~~~~~~~~~~~")
+
+function verificaIncluziuneArray (arrayMare, arrayMic) {
+
+    for (let i = 0; i < arrayMic.length; i++) {
+        if (!arrayMare.includes (arrayMic [i])) {  
+            return false;
+        }
+    }
+    return true;
+}
+
+let arrayMare = array;
+let arrayMic = [1, 2, 3]; 
+console.log(verificaIncluziuneArray (arrayMare, arrayMic));
+
+arrayMic = [1, 2, 13];
+console.log(verificaIncluziuneArray (arrayMare, arrayMic));
+
+arrayMic = [10, 13];
+console.log(verificaIncluziuneArray (arrayMare, arrayMic));
+
+
